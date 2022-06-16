542fea4553f98acad50ab9c9379bf0c263e9e334
Ananay Sood <105236001+panshu02@users.noreply.github.com>
2022-06-16 09:40:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from model.train import train
 from model.predict_results import predict_result
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+import datetime
 
 if __name__ == '__main__':
     urls = dict()
@@ -16,13 +19,12 @@ if __name__ == '__main__':
     for commodity in urls:
         url = urls[commodity]
         data = give_df(url)
-        print("\n**\t{} past trends\t**\n\n".format(commodity))
-        data = give_df(url)
+        print("\n**\t{}\t**\n\n".format(commodity))
 
         n_time_lags = 4
         df, X_cols = build(data, 4)
         print("Pre-processed training set:\n")
-        print(df)
+        #print(df)
         sc1, sc2, X, Y = trainset(df, X_cols)
 
         mlp = train(X, Y)
@@ -42,7 +44,7 @@ if __name__ == '__main__':
         result = mlp.predict(next_day)
         result = sc2.inverse_transform(result)
         print("\n Prediction for price and volume for tommorow:")
-        print("Price($) : {}\t\t Volume(Barrels) : {}\n".format(result[0][1], result[0][0]))
+        print("Price($) : {}\t\t Volume : {}\n".format(result[0][1], result[0][0]))
         next_day = sc1.inverse_transform(next_day)
         #print(next_day)
 
@@ -53,3 +55,36 @@ if __name__ == '__main__':
         results = predict_result(n_days, results, cols, mlp, sc1, sc2)
         print("Price and Volume predictions for the next {} days:\n".format(n_days))
         print(results.loc[:, ['Price', 'Vol']])
+
+
+        # Plotting Results
+        X_axis = df['Date'].values
+        month_dic = {'Jan' : 1, 'Feb' : 2, 'Mar' : 3, 'Apr' : 4, 'May' : 5, 'Jun' : 6, 'Jul' : 7, 'Aug' : 8, 'Sep' : 9, 
+        'Oct' : 10, 'Nov' : 11, "Dec" : 12}
+
+        for i in range(len(X_axis)):
+            date = X_axis[i].split()
+            (year, month, date) = (int(date[-1]), month_dic[date[0]], int(date[1][:-1]))
+            date = datetime.datetime(year, month, date)
+            X_axis[i] = date
+        
+        last_date = X_axis[-1]
+        X_axis_1 = []
+        for i in range(n_days):
+            date = last_date+datetime.timedelta(days = i)
+            X_axis_1.append(date)
+        
+        plt.plot(X_axis, df['Price'].values, color = 'red', alpha=0.7)
+        plt.xlabel('Dates')
+        plt.xticks([])
+        plt.ylabel("Price in $")
+        plt.title("Original Price vs Date ")
+        plt.show()
+
+        plt.plot(X_axis_1, results['Price'].values, color = 'blue', alpha = 0.7)
+        plt.xlabel('Dates')
+        plt.xticks([])
+        plt.ylabel("Price in $")
+        plt.title("Predicted Price vs Date ")
+        plt.show()
+        print('\n')
