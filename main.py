18981f4ed11bfc1320949f0d7335f0b15f53b562
Ananay Sood <105236001+panshu02@users.noreply.github.com>
2022-06-16 08:36:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,40 +7,49 @@ import pandas as pd
 import numpy as np
 
 if __name__ == '__main__':
-    url = 'https://www.investing.com/commodities/crude-oil-historical-data'
-    data = give_df(url)
-    n_time_lags = 4
-    df, X_cols = build(data, 4)
-    print("Pre-processed training set:\n")
-    print(df)
-    sc1, sc2, X, Y = trainset(df, X_cols)
+    urls = dict()
+    urls['Crude Oil'] = 'https://www.investing.com/commodities/crude-oil-historical-data'
+    urls['Natural Gas'] = 'https://www.investing.com/commodities/natural-gas-historical-data'
+    urls['Gold'] = 'https://www.investing.com/commodities/gold-historical-data'
+    urls['Bitcoin'] = 'https://www.investing.com/crypto/bitcoin/historical-data'
+    urls['Ethereum'] = 'https://www.investing.com/crypto/ethereum/historical-data'
+    for commodity in urls:
+        url = urls[commodity]
+        data = give_df(url)
+        print("\n**\t{} past trends\t**\n\n".format(commodity))
+        data = give_df(url)
 
-    mlp = train(X, Y)
+        n_time_lags = 4
+        df, X_cols = build(data, 4)
+        print("Pre-processed training set:\n")
+        print(df)
+        sc1, sc2, X, Y = trainset(df, X_cols)
 
-    n_days = 3
+        mlp = train(X, Y)
 
-    cols = ['Vol', 'Price']
-    for i in range(1, n_time_lags):
-        cols.append('Vol({})'.format(i))
-        cols.append('Price({})'.format(i))
+        cols = ['Vol', 'Price']
+        for i in range(1, n_time_lags):
+            cols.append('Vol({})'.format(i))
+            cols.append('Price({})'.format(i))
 
-    next_day = df.loc[0, cols].values
-    #print(next_day)
-    next_day = sc1.transform([next_day])
+        next_day = df.loc[0, cols].values
+        #print(next_day)
+        next_day = sc1.transform([next_day])
 
-    results = pd.DataFrame(columns = df.columns[1:])
-    #print(len(results.columns))
+        results = pd.DataFrame(columns = df.columns[1:])
+        #print(len(results.columns))
 
-    result = mlp.predict(next_day)
-    result = sc2.inverse_transform(result)
-    print("\n Result for the price and volume for tommorow:")
-    print("Price($) : {}\t\t Volume(Barrels) : {}\n".format(result[0][1], result[0][0]))
-    next_day = sc1.inverse_transform(next_day)
-    #print(next_day)
+        result = mlp.predict(next_day)
+        result = sc2.inverse_transform(result)
+        print("\n Prediction for price and volume for tommorow:")
+        print("Price($) : {}\t\t Volume(Barrels) : {}\n".format(result[0][1], result[0][0]))
+        next_day = sc1.inverse_transform(next_day)
+        #print(next_day)
 
-    results.loc[0] = np.concatenate((result[0], next_day[0]))
-    #print(results)
+        results.loc[0] = np.concatenate((result[0], next_day[0]))
+        #print(results)
 
-    results = predict_result(n_days, results, cols, mlp, sc1, sc2)
-    print("Price and Volume list for the next {} days:\n".format(n_days))
-    print(results.loc[:, ['Price', 'Vol']])
+        n_days = 3
+        results = predict_result(n_days, results, cols, mlp, sc1, sc2)
+        print("Price and Volume predictions for the next {} days:\n".format(n_days))
+        print(results.loc[:, ['Price', 'Vol']])
